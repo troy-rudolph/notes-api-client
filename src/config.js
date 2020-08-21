7834a8dd03627e6cad28b17c8be59f220836fa24
trudolph <trudolph@TRUDOLPH5400.corp.alchemysystems.com>
2020-08-21 12:42:46 -0500

    after https://branchv41--serverless-stack.netlify.app/chapters/configure-aws-amplify.html

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "buckbranchranch"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "buckbranchranch"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_huseqtLkR",
+    APP_CLIENT_ID: "k7ggis3rcsqt5ll4ar3e5mdki",
+    IDENTITY_POOL_ID: "us-east-2:d52acc4e-88d7-4086-81c6-7b68b66172c2"
+  }
+};
